2ec490cc741ee4e2fdd4cd1dc21e9902c8f48ff9
jd2698 <j.delgado2698@gmail.com>
2024-05-19 13:34:07 -0500

    se agregó los estilos de pokemonView

--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -7,16 +7,24 @@
 	const { data, error, loading, getData } = useGetData();
 	const route = useRoute();
 	const router = useRouter();
+	const sprites = ref(null);
 
 	onMounted(async () => {
-		getData(`https://pokeapi.co/api/v2/pokemon/${route.params.name}`);
+		await getData(`https://pokeapi.co/api/v2/pokemon/${route.params.name}`);
+
+		if (error.value == null) {
+			sprites.value = data.value.sprites.other.dream_world.front_default;
+		}
 	});
 </script>
 <template>
 	<Spinner v-if="loading" />
-	<div v-if="data">
-		<img :src="data.sprites.front_shiny" alt="">
-		<span>{{data.name}}</span>
+	<div v-if="error">{{error}}</div>
+
+	<div v-if="data" class="w-[240px] m-auto p-4 flex flex-col gap-2 rounded-lg shadow-[0_0_6px_0_#43ba83]">
+		<span class="text-end">{{data.name.toUpperCase()}}</span>
+		<img :src="sprites" :alt="data.name" class="h-[200px] w-full object-contain">
+		<span>type: {{data.types[0].type.name}}</span>
 	</div>
 	<!-- <h1>Poke: {{ $route.params.name }}</h1> -->
 </template>
