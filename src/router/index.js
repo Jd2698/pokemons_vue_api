4f74a9540c61887593b71b7d2505a22aa2b78f96
jd2698 <j.delgado2698@gmail.com>
2024-05-22 18:11:28 -0500

    se agregó ruta notFound

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,11 @@ const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
 		{
+			path: '/:pathMatch(.*)*',
+			name: 'NotFound',
+			component: () => import('../views/HomeView.vue')
+		},
+		{
 			path: '/',
 			name: 'home',
 			component: () => import('../views/HomeView.vue')
